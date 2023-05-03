40c09a0e734779c3c251b14c320a9fa6975d7bbe
C. Molendijk <cmolendijk@users.noreply.github.com>
2023-05-03 21:33:13 +0200

    Added GA4 tag

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -4,12 +4,30 @@ import type { AppProps } from 'next/app';
 import './utils/globals.css';
 
 import { Layout } from '@src/components/templates/layout';
+import Script from 'next/script';
 
 const urbanist = Urbanist({ subsets: ['latin'], variable: '--font-urbanist' });
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-8B1HQSNPTR`}
+      />
+      <Script
+        id="G-8B1HQSNPTR"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-8B1HQSNPTR');
+          `,
+        }}
+      />
       <main className={`${urbanist.variable} font-sans`}>
         <Layout>
           <Component {...pageProps} />
